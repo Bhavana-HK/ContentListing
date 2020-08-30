fc9d16b4c7d54d6a41df0bc47bdca7c4fcce87c5
Krishnamurthy <krishnamurthyb@win.dowjones.net>
2020-08-30 20:56:19 +0530

    Updates

---
+++ precache-manifest.3c435e1757cf27c899b896717412fd81.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5f4973a44f589993db37a8140a957e7",
+    "url": "/ContentListing/index.html"
+  },
+  {
+    "revision": "4aaf3efe9aad18b89a70",
+    "url": "/ContentListing/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1ea4bd111214bc332f84",
+    "url": "/ContentListing/static/js/2.44ba9e06.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/ContentListing/static/js/2.44ba9e06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4aaf3efe9aad18b89a70",
+    "url": "/ContentListing/static/js/main.5b8da9dc.chunk.js"
+  },
+  {
+    "revision": "58795fe855029644255c",
+    "url": "/ContentListing/static/js/runtime-main.152cd70e.js"
+  },
+  {
+    "revision": "d5f2a7f493e94ed267d41620eef143d9",
+    "url": "/ContentListing/static/media/poster1.d5f2a7f4.jpg"
+  },
+  {
+    "revision": "81ccf144677486906fcf751450652895",
+    "url": "/ContentListing/static/media/poster2.81ccf144.jpg"
+  },
+  {
+    "revision": "e242f0372e7bdbf2e48aa5afefc72b97",
+    "url": "/ContentListing/static/media/poster3.e242f037.jpg"
+  },
+  {
+    "revision": "56f097245c605a5d312f103dedb21bcb",
+    "url": "/ContentListing/static/media/poster4.56f09724.jpg"
+  },
+  {
+    "revision": "5ef7f79461c7a8c0a95652b813fb4722",
+    "url": "/ContentListing/static/media/poster5.5ef7f794.jpg"
+  },
+  {
+    "revision": "164b05a3cd770650dd30bc71f5407e99",
+    "url": "/ContentListing/static/media/poster6.164b05a3.jpg"
+  },
+  {
+    "revision": "23ac494a92660204d6b8acbba20aa0e9",
+    "url": "/ContentListing/static/media/poster7.23ac494a.jpg"
+  },
+  {
+    "revision": "ef89f612f6e3d684892d6525b7957ce4",
+    "url": "/ContentListing/static/media/poster8.ef89f612.jpg"
+  },
+  {
+    "revision": "65d6d5002a2f98860f29d3c61599c5ac",
+    "url": "/ContentListing/static/media/poster9.65d6d500.jpg"
+  }
+]);
